5cbbd8a08ec3f5cef34be5178ea9ec3d2c014a17
tonyarauj0 <tonyaraujoce@gmail.com>
2022-07-08 11:36:44 -0300

    Dados brutos financiamento campanha

---
+++ code/2_baixar_dados_financiamento.R
@@ -0,0 +1,25 @@
+# Baixar dados do financiamento de campanha
+
+# Pacotes
+
+install.packages("pacman")
+pacman::p_load(tidyverse, basedosdados)
+
+# 1 Baixar dados através do pacote basedosdados ----
+# 1.1 id de projeto no Big Query
+basedosdados::set_billing_id("projeto-base-dados")
+
+# 1.2. Montar query
+query <- "SELECT *
+FROM `basedosdados.br_tse_eleicoes.receitas_candidato`
+WHERE ano = 2014
+AND cargo = 'deputado federal'"
+
+# 1.3 Atribuindo a um data frame
+fc_dep_fed_14 <- basedosdados::read_sql(query)
+
+
+# Salvar ------------------------------------------------------------------
+saveRDS(fc_dep_fed_14,
+        file = here::here("data", "clean", "financiamento.rds"),
+        compress = T)
