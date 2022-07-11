bc28fab83170f39c00b265bcfc45100f832b5cc2
tonyarauj0 <tonyaraujoce@gmail.com>
2022-07-11 19:55:11 -0300

    baixar dados pessoais candidatos deputado

---
+++ code/9_dados_pessoais_candidatos_deputado.R
@@ -0,0 +1,20 @@
+# Baixar dados pessoais dos candidatos a deputado
+
+# 1 Baixar dados através do pacote basedosdados ----
+# 1.1 id de projeto no Big Query
+basedosdados::set_billing_id("projeto-base-dados")
+
+# 1.2. Montar query
+query <- "SELECT * FROM `basedosdados.br_tse_eleicoes.candidatos`
+WHERE ano = 2014 AND cargo = 'deputado federal'
+AND situacao = 'deferido' OR
+situacao = 'deferido com recurso'"
+
+# 1.3 Atribuindo a um data frame
+dados_pessoais_cand_deputado <- basedosdados::read_sql(query)
+
+
+# Salvar ------------------------------------------------------------------
+saveRDS(dados_pessoais_cand_deputado,
+        file = here::here("data", "raw", "dados_pessoais.rds"),
+        compress = T)
