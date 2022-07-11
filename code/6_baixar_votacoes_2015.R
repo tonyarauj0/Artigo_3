293095f5faef20f15eb91e40c9cebd6f24923411
tonyarauj0 <tonyaraujoce@gmail.com>
2022-07-11 09:29:39 -0300

    baixar votacoes nominais de 2015

--- code/6_baixar_votacoes_2015.R
+++ code/6_baixar_votacoes_2015.R
@@ -5,10 +5,10 @@ devtools::install_github("RobertMyles/congressbr")
 
 
 # Baixar as votacoes em 2015 ---------------------------------------
-votacoes_15 <- congressbr::cham_votes_year(2015)
+votacoes_nominais_2015 <- congressbr::cham_votes_year(2015)
 
 # Salvar ------------------------------------------------------------------
-saveRDS(votacoes_15,
+saveRDS(votacoes_nominais_2015,
         file = here::here("data", "raw", "votacoes_2015.rds"),
         compress = T)
 
