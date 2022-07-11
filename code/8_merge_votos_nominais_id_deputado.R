0972fa917260e15d60971fc01eae582aadcf8fef
tonyarauj0 <tonyaraujoce@gmail.com>
2022-07-11 18:23:40 -0300

    Merge votacoes nominais sobre financiamento (pec182) e cpfs dos deputados.

---
+++ code/8_merge_votos_nominais_id_deputado.R
@@ -0,0 +1,18 @@
+# Filtrar deputados de interesse e casar com demais bases
+
+
+# 1 Carregar as votacoes nominais ligadas ao financiamento de campanha --------
+
+votacoes_nominais_financiamento <- readRDS( file = here::here("data", "clean", "votacoes_financiamento.rds"))
+
+# 2 Carregar ids e cpfs dos Deputados -----
+id_cpf_dep <- readRDS(file = here::here("data", "raw", "identificador_deputados.rds"))
+
+
+# 3. Merge id-cpf para casar com demais bases -------------------------------
+
+base <- votacoes_nominais_financiamento |>
+    dplyr::select(- c(deputado_uri_partido, deputado_uri, deputado_id_legislatura, deputado_url_foto)) |>
+    dplyr::left_join(id_cpf_dep, by = c("deputado_id" = "id"))
+
+
