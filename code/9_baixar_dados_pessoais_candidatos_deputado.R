214680450573059812d6502ca30b3d9d49fcd3c3
tonyarauj0 <tonyaraujoce@gmail.com>
2022-07-13 06:53:58 -0300

    atualizacao sigla dos partidos

--- code/9_baixar_dados_pessoais_candidatos_deputado.R
+++ code/9_baixar_dados_pessoais_candidatos_deputado.R
@@ -13,6 +13,18 @@ WHERE ano = 2014 AND cargo = 'deputado federal'
 dados_pessoais_cand_deputado <- basedosdados::read_sql(query)
 
 
+
+# 2. Modificar Sigla do partido -------------------------------------------
+dados_pessoais_cand_deputado <- dados_pessoais_cand_deputado |>
+    dplyr::mutate(sigla_partido = dplyr::case_when(
+        sigla_partido == "PATRIOTA" ~ "PEN",
+        sigla_partido == "SOLIDARIEDADE" ~ "SD",
+        sigla_partido == "PODE" ~ "PTN",
+        TRUE ~ sigla_partido
+    ))
+
+
+
 # Salvar ------------------------------------------------------------------
 saveRDS(dados_pessoais_cand_deputado,
         file = here::here("data", "raw", "dados_pessoais.rds"),
