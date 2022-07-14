ad4a46f0b4f72a310544d9490c669b90440438e1
tonyarauj0 <tonyaraujoce@gmail.com>
2022-07-14 11:17:30 -0300

    graficos de variaveis politicas

--- code/13.4_graficos_variaveis políticas.R
+++ code/13.4_graficos_variaveis políticas.R
@@ -43,6 +43,8 @@ base.1 <- dados |>
      )
 )
 
+#obs: 100% = 458 (votaram)
+
 ggplot2::ggsave(
     filename = here::here("figures", "dados", "politicos", "orientacao.png"),
     plot = barras_orientacao ,
@@ -52,8 +54,8 @@ ggplot2::ggsave(
 )
 
 
-# 2. Governo x Oposição --------------------------------------
-(barras_orientacao <-
+# 3. Governo x Oposição --------------------------------------
+(barras_governo <-
      base.1 |>
      count(ema22, governo, sort = T) |>
      mutate(orientacao = tidytext::reorder_within(governo, n, ema22))  |>
@@ -61,9 +63,9 @@ ggplot2::ggsave(
      geom_col(show.legend = FALSE, alpha = 0.8) +
      geom_label(aes(label = n), size = 5,color = "white", show.legend = F) +
      tidytext::scale_y_reordered() +
-     facet_wrap(~ema22, scales = "free") +
-     labs(x = "", y = "ORIENTAÇÃO PARTIDÁRIA", caption = "Nota: valores absolutos dentro das barras.") +
-     scale_fill_manual(values = c("#A9A9A9", "firebrick", "dodgerblue4")) +
+     facet_wrap(~ema22, scales = "free_y") +
+     labs(x = "", y = "", caption = "Nota: Valores absolutos dentro das barras.") +
+     scale_fill_manual(values = c("firebrick", "dodgerblue4")) +
      theme_minimal() +
      theme(plot.caption.position = "plot",
            plot.caption = element_text(hjust = 0)) +
@@ -77,9 +79,72 @@ ggplot2::ggsave(
 )
 
 ggplot2::ggsave(
-    filename = here::here("figures", "dados", "politicos", "orientacao.png"),
-    plot = barras_orientacao ,
+    filename = here::here("figures", "dados", "politicos", "governo.png"),
+    plot = barras_governo ,
     dpi = 600,
     width = 8,
     height = 6
 )
+
+# 4. Boxplot votos --------------------------------------
+rain_height <- .1
+(gr_votos <- base.1 |> select(deputado_id, votos, ema22) |>
+        ggplot(aes(x = "", y = log10(votos), fill = ema22)) +
+        introdataviz::geom_flat_violin(trim=FALSE, alpha = 0.4,
+                                       position = position_nudge(x = rain_height+.05)) +
+        geom_boxplot(width = rain_height, alpha = 0.4, show.legend = FALSE,
+                     outlier.shape = NA,
+                     position = position_nudge(x = -rain_height*2)) +
+        stat_summary(fun.data = mean_cl_normal, mapping = aes(color = ema22), show.legend = FALSE,
+                     position = position_nudge(x = rain_height * 3)) +
+        scale_x_discrete(name = "", expand = c(rain_height*3, 0, 0, 0.7)) +
+        coord_flip() +
+        scale_fill_manual(values = c("firebrick", "dodgerblue4")) +
+        scale_colour_manual(values = c("firebrick", "dodgerblue4")) +
+        theme_minimal() +
+        labs(fill = "", y = "") +
+        theme(panel.grid.major.y = element_blank(),
+              legend.background = element_rect(fill = "white", color = "white"))
+)
+
+ggplot2::ggsave(
+    filename = here::here("figures", "dados", "politicos", "votos.png"),
+    plot = gr_votos ,
+    dpi = 600,
+    width = 8,
+    height = 6
+)
+
+# 3. situacao eleicao --------------------------------------
+(barras_situacao <-
+     base.1 |>
+     count(ema22, resultado, sort = T) |>
+     mutate(orientacao = tidytext::reorder_within(resultado, n, ema22))  |>
+     ggplot(aes(x = (n/sum(n))*100, y = orientacao, fill = ema22)) +
+     geom_col(show.legend = FALSE, alpha = 0.8) +
+     geom_label(aes(label = n), size = 5,color = "white", show.legend = F) +
+     tidytext::scale_y_reordered() +
+     facet_wrap(~ema22, scales = "free_y") +
+     labs(x = "", y = "", caption = "Nota: Valores absolutos dentro das barras.") +
+     scale_fill_manual(values = c("firebrick", "dodgerblue4")) +
+     theme_minimal() +
+     theme(plot.caption.position = "plot",
+           plot.caption = element_text(hjust = 0)) +
+     scale_x_continuous(labels = function(x) paste(format(x,
+                                                          big.mark = ".",
+                                                          decimal.mark = ",",
+                                                          scientific = FALSE),
+                                                   "%"
+     )
+     )
+)
+
+
+ggplot2::ggsave(
+    filename = here::here("figures", "dados", "politicos", "situacao.png"),
+    plot = barras_situacao ,
+    dpi = 600,
+    width = 8,
+    height = 6
+)
+
